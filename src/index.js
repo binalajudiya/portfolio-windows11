3633c63b8d823969d28bafeea1c173acea4d4bdd
Binal <binalajudiya.ba@gmail.com>
2023-05-22 19:12:18 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,22 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-function counterRedurce(state={value: 0}, action){
-    switch(action.type){
-        case 'increment':
-         return {value: state.value+1}
-        case 'decrement':
-         return {value: state.value-1}
-        default:
-            return state
-    }
-}
-
-let store = createStore(counterRedurce)
-
-store.subscribe(()=>console.log(store.getState()))
-
-store.dispatch({type:'increment'})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
