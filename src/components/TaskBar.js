de3494f0701841acd6ec394f7a255b6e8d3a8b45
Binal <binalajudiya.ba@gmail.com>
2023-06-08 20:13:12 +0100

    added all functions

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -3,14 +3,31 @@ import { AiOutlineWifi } from "react-icons/ai";
 import { GiSpeaker } from "react-icons/gi";
 import TaskBarPinAppsButtons from "./TaskBarPinAppsButtons";
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import moment from "moment";
+import { motion } from 'framer-motion';
+
 
 
 const TaskBar = () => {
 
     const [date, setDay] = useState(moment().format("DD[/]MM[/]YYYY"));
-    const [time, setTime] = useState(moment().format("HH:MM"))
+    const [time, setTime] = useState(moment().format("HH:MM"));
+    const [datetime, setDateTime] = useState(moment().format("dddd, DD MMMM YYYY"));
+    const [dateToggled, setdateToggled] = useState(false);
+
+    const _updatedTime = () => {
+        return setInterval(() => {
+            setDay(moment().format("DD[/]MM[/]YYYY"));
+            setTime(moment().format("HH:MM"));
+            setDateTime(moment().format("dddd DD MMMM YYYY"));
+        },1000);
+    }
+
+    useEffect(() => {
+        const current = _updatedTime();
+        return() => clearInterval(current);
+    },[])
 
     const pinApps = useSelector((state) => state.desktopScreen.pinApps);
 
@@ -53,7 +70,10 @@ const TaskBar = () => {
                         </div>
                     </div>
                     <div className="relative">
-                        <div className="flex m-[.1em] h-[2.4em] border-black rounded-md hover:bg-white hover:bg-opacity-10 hover:backdrop-blur-xl hover:backdrop-filter">
+                        <div className="flex m-[.1em] h-[2.4em] border-black rounded-md hover:bg-white hover:bg-opacity-10 hover:backdrop-blur-xl hover:backdrop-filter"
+                        onClick={() => {
+                            setdateToggled((prev) => !(prev))
+                        }}>
                             <div className="flex justify-center items-center px-2">
                                 <div className="flex flex-col items-end justify-center text-xs">
                                     <span>{time}</span>
@@ -61,6 +81,16 @@ const TaskBar = () => {
                                 </div>
                             </div>
                         </div>
+                        {dateToggled && (
+                            <motion.div
+                                initial = {{right: -1000}} 
+                                animate = {{right: 0}}
+                                className="absolute -top-[50px] m-2 -right-0 z-10 border-[1px] border-gray-100 rounded-mdborder-white bg-transparent text-sm " >
+                                    <div className="flex flex-row justify-center text-center items-center">
+                                        <span className="p-2 h-10 w-70">{datetime}</span>
+                                    </div>
+                            </motion.div>
+                        )}
                     </div>
                 </div>
             </div>
